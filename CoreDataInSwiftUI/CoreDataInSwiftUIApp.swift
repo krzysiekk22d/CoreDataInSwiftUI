99de6b1bae9bf188f58d98b70fad3a184f060c5c
krzysiekk22d <klas.club@gmail.com>
2023-09-19 05:13:31 +0200

    Initial Commit

---
+++ CoreDataInSwiftUI/CoreDataInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataInSwiftUIApp.swift
+//  CoreDataInSwiftUI
+//
+//  Created by Krzysztof Czura on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
